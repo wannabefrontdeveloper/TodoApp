1ef4f602074f7b3e9a56bff2d5be81267d8feb7d
wannabefrontdeveloper <giwonk@naver.com>
2024-02-07 10:40:28 +0900

    코드추가 스타일로 Image 컴포넌트의 크기를 300dp X 200dp로 설정하였습니다.

--- components/Empty.js
+++ components/Empty.js
@@ -4,7 +4,10 @@ import {View, Text, StyleSheet, Image} from 'react-native';
 function Empty() {
   return (
     <View style={styles.block}>
-      <Image source={require('../assets/images/circle.png')} />
+      <Image
+        source={require('../assets/images/circle.png')}
+        style={styles.image}
+      />
       <Text style={styles.description}>야호! 할일이 없습니다.</Text>
     </View>
   );
@@ -15,6 +18,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 300,
+    height: 200,
   },
   description: {
     fonstSize: 24,
