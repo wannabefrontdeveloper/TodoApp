ffc8e792107758961315eb9dea2c72b1fd040401
wannabefrontdeveloper <giwonk@naver.com>
2024-02-08 12:16:53 +0900

    코드수정 할일을 등록할 수 있게끔 하였습니다.

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -10,9 +10,10 @@ import {
   Keyboard,
 } from 'react-native';
 
-function AddTodo() {
+function AddTodo({onInsert}) {
   const [text, setText] = useState('');
   const onPress = () => {
+    onInsert(text);
     setText('');
     Keyboard.dismiss();
   };
